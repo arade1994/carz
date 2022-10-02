fd2ec0eec5148a0000bfd81ed7324ce71d8cef4c
Ante Rade <anterade99@gmail.com>
2022-10-02 21:37:24 +0200

    Implemented signing user out

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -46,6 +46,11 @@ export class UsersController {
         return user;
     }
 
+    @Post('/signout')
+    signout(@Session() session: any) {
+        session.userId = null;
+    }
+
     @Get('/:id')
     async findUser(@Param('id') id: string) {
         const user = await this.usersService.findOne(parseInt(id));
