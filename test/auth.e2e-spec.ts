e8a4c87f97c292393d5c2d1691a50d600b24536f
Ante Rade <anterade99@gmail.com>
2022-10-09 22:39:15 +0200

    NestJS way of wirring global pipe and cookie-session

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -2,7 +2,6 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
-import { setupApp } from '../src/setup-app';
 
 describe('Authentication system', () => {
     let app: INestApplication;
@@ -13,12 +12,11 @@ describe('Authentication system', () => {
         }).compile();
 
         app = moduleFixture.createNestApplication();
-        setupApp(app);
         await app.init();
     });
 
     it('handles user signup', () => {
-        const email = 'ngsrgkosft@pagns.com';
+        const email = 'ngsrgkossfft@pagns.com';
 
         return request(app.getHttpServer())
             .post('/auth/signup')
