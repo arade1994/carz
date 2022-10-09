8ccc8d6f8968524603dc3c7603f3fd9686181a5d
Ante Rade <anterade99@gmail.com>
2022-10-09 14:30:50 +0200

    Improved implementation of fake users service; updated all tests; created test for signing user in

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,5 +1,8 @@
 import { Test } from '@nestjs/testing';
-import { BadRequestException } from '@nestjs/common/exceptions';
+import {
+    BadRequestException,
+    NotFoundException,
+} from '@nestjs/common/exceptions';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 import User from './user.entity';
@@ -9,10 +12,23 @@ describe('AuthService', () => {
     let fakeUsersService: Partial<UsersService>;
 
     beforeEach(async () => {
+        const users: User[] = [];
         fakeUsersService = {
-            find: () => Promise.resolve([]),
-            create: (email: string, password: string) =>
-                Promise.resolve({ id: 1, email, password } as User),
+            find: (email: string) => {
+                const filteredUsers = users.filter(
+                    (user) => user.email === email
+                );
+                return Promise.resolve(filteredUsers);
+            },
+            create: (email: string, password: string) => {
+                const user = {
+                    id: Math.floor(Math.random() * 999999),
+                    email,
+                    password,
+                } as User;
+                users.push(user);
+                return Promise.resolve(user);
+            },
         };
 
         const module = await Test.createTestingModule({
@@ -28,24 +44,47 @@ describe('AuthService', () => {
         service = module.get(AuthService);
     });
 
-    it('creates and instance of auth service', async () => {
+    it('creates an instance of auth service', async () => {
         expect(service).toBeDefined();
     });
 
-    it('creates a new user with hashed password', async () => {
-        const user = await service.signup('anterade99@gmail.com', '123456');
-        expect(user).toBeDefined();
+    describe('signup()', () => {
+        it('creates a new user with hashed password', async () => {
+            const user = await service.signup('test@test.com', '123456');
+            expect(user).toBeDefined();
 
-        const [salt, hash] = user.password.split('.');
-        expect(salt).toBeDefined();
-        expect(hash).toBeDefined();
+            const [salt, hash] = user.password.split('.');
+            expect(salt).toBeDefined();
+            expect(hash).toBeDefined();
+        });
+
+        it('throws an error when user tries to signup with existing email address', async () => {
+            await service.signup('test@test.com', 'test');
+            await expect(
+                service.signup('test@test.com', '123456')
+            ).rejects.toThrow(BadRequestException);
+        });
     });
 
-    it('throws and error when user tries to signup with existing email address', async () => {
-        fakeUsersService.find = () =>
-            Promise.resolve([{ id: 1, email: 'a', password: 's' } as User]);
-        await expect(
-            service.signup('anterade99@gmail.com', '123456')
-        ).rejects.toThrow(BadRequestException);
+    describe('signin()', () => {
+        it('throws an error if user signs in with unused email', async () => {
+            await expect(
+                service.signin('test@test.com', 'test')
+            ).rejects.toThrow(NotFoundException);
+        });
+
+        it('throws an error if user signs in with invalid password', async () => {
+            await service.signup('test@test.com', 'test');
+            await expect(
+                service.signin('test@test.com', 'adgsrgd')
+            ).rejects.toThrow(BadRequestException);
+        });
+
+        it('signs in a user with correct password supplied', async () => {
+            await service.signup('test@test.com', 'test');
+            const user = await service.signin('test@test.com', 'test');
+
+            expect(user).toBeDefined();
+        });
     });
 });
