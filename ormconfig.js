8a9f380fce47980ecbf2828b17045e7f52449b90
Ante Rade <anterade99@gmail.com>
2022-10-19 21:04:52 +0200

    Configuring typeorm for all environments and prepared app for production deployment

---
+++ ormconfig.js
@@ -0,0 +1,40 @@
+var dbConfig = {
+    synchronize: false,
+    migrations: ['migrations/*.js'],
+    cli: {
+        migrationsDir: 'migrations'
+    }
+}
+
+switch(process.env.NODE_ENV) {
+    case 'development':
+        Object.assign(dbConfig, {
+            type: 'sqlite',
+            database: 'db.sqlite',
+            entities: ['**/*.entity.js']
+        })
+        break;
+    case 'test':
+        Object.assign(dbConfig, {
+            type: 'sqlite',
+            database: 'test.sqlite',
+            entities: ['**/*.entity.ts'],
+            migrationsRun: true
+        })
+        break;
+    case 'production':
+        Object.assign(dbConfig, {
+            type: 'postgress',
+            url: process.env.DATABASE_URL, //for deployment to Heroku
+            migrationsRun: true,
+            entities: ['**/*.entity.js'],
+            ssl: {
+                rejectUnauthorized: false
+            }
+        })
+        break;
+    default:
+        throw new Error('unknown environment')
+}
+
+module.exports = dbConfig;
--- src/app.module.ts
+++ src/app.module.ts
@@ -7,8 +7,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
-import User from './users/user.entity';
-import Report from './reports/report.entity';
 
 const cookieSession = require('cookie-session');
 
@@ -18,15 +16,7 @@ const cookieSession = require('cookie-session');
             isGlobal: true,
             envFilePath: `.env.${process.env.NODE_ENV}`,
         }),
-        TypeOrmModule.forRootAsync({
-            inject: [ConfigService],
-            useFactory: (config: ConfigService) => ({
-                type: 'sqlite',
-                database: config.get<string>('DB_NAME'),
-                entities: [User, Report],
-                synchronize: true,
-            }),
-        }),
+        TypeOrmModule.forRoot(),
         UsersModule,
         ReportsModule,
     ],
